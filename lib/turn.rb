bdd79f0cdd66dd3be1011c3c05046a8c9749aaf9
Eric Ontiveros <ericontiveros123@gmail.com>
2021-04-15 20:49:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,5 +27,5 @@ def turn(board)
   user_input = gets.strip
   input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
+    move(board, index, current_player
 end
